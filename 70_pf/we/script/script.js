4c4581dec9a69e3ae1915fd1655d802725407e5e
angel144000 <angel144000@naver.com>
2024-08-13 16:27:29 +0900

    0813

---
+++ 70_pf/we/script/script.js
@@ -0,0 +1,13 @@
+// 준비 이벤트
+$(()=>{
+    // 모달 팝업
+
+    // 공지사항의 첫 번째 li를 클릭하면
+    $(".notice li").eq(0).click(()=>{
+        $("#modal").css({display: "block"});
+    });
+
+    $("button").click(()=>{
+        $("#modal").css({display: "none"});
+    });
+});//모달 팝업 끝
