3ef8b3d5bb48b33b1a3aade5fc3a9f5997dcecb2
angel144000 <angel144000@naver.com>
2024-08-21 17:19:56 +0900

    0821

---
+++ 70_pf/snuh_v2/js/mdata.js
@@ -0,0 +1,25 @@
+const mdata = {
+    // 영어 데이터
+    en: {
+        gnb: ["Specialization", "medical staff", "CS Center", "Notice"],
+        mtit: ["SNUH", "University Hospital"],
+        stit: ["", "Specialization","medical staff", "CS center"],
+        s2: {
+            img: ["assets/a4.jpg","assets/a3.jpg","assets/a2.jpg","assets/a1.jpg","assets/a8.jpg","assets/a7.jpg","assets/a6.jpg","assets/a5.jpg"],
+            tit: ["cancer center", "brain nerve center", "Senior Medical Center", "Cardiovascular Center", "lung center", "joint center", "Spine Center", "medicine Center"]
+        }  
+    },
+    // 한국어 데이터
+    ko: {
+        gnb: ["특성화센터", "의료진", "고객센터", "공지사항"],
+        mtit: ["SNUH", "서울대병원"],
+        stit: ["특성화센터", "의료진", "고객센터"],
+        s2: {
+            img: ["assets/a1.jpg","assets/a2.jpg","assets/a3.jpg","assets/a4.jpg","assets/a5.jpg","assets/a6.jpg","assets/a7.jpg","assets/a8.jpg"],
+            tit: ["암센터","뇌신경센터", "노인의료센터", "심장혈관센터", "페센터", "관절센터", "척추센터", "소화기센터"]
+        }  
+    },
+};
+
+// 기본 데이터 내보내기 - 1개만
+export default mdata;
