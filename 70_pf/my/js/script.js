2d294337e858cf391fa53df699f3e8f7a8b7fada
angel144000 <angel144000@naver.com>
2024-09-06 12:21:39 +0900

    0906

--- 70_pf/my/js/script.js
+++ 70_pf/my/js/script.js
@@ -1,4 +1,40 @@
 $(() => {
+    // video player
+    /* 
+        비디오 가져오기: 객체.get(0)
+        비디오 플레이: 객체.play()
+        비디오 일시정지: 객체.pause()
+        비디오 정지: 객체.load()
+    */
+
+    const vid = $("#s2 .vid video").get(0);
+    const vidBtn = $("#s2 .vid i");
+
+    // toggle 동작 만들기
+    // 하나의 버튼으로 두 개의 상태를 만든다.
+    let flag = 0;
+
+    vidBtn.click((e)=>{
+        if (flag === 0) {
+            vid.play();
+            $(e.currentTarget).attr({class: "fa-regular fa-circle-pause"});
+            flag = 1;
+        } else {
+            vid.pause();
+            $(e.currentTarget).attr({class: "fa-regular fa-circle-play"});
+            flag = 0;
+        }
+    });
+
+    // audio button effect
+    const btn = $(".btn");
+    const snd = $(".snd").get(0);
+
+    btn.mouseenter(()=>{
+        snd.play();
+    });
+
+
     // progress bar & counter
     // 요소 찾기
     const cntNumEle = $(".counter b");
@@ -88,20 +124,21 @@ $(() => {
 
     // lightbox2 link
     const lb_caption = $(".lb-caption");
-    lb_caption.css({ cursor: "pointer" });
+    lb_caption.css({cursor: "pointer"});
     lb_caption.click(() => {
         const lb_link = $(event.currentTarget).text();
-        $(location).attr({ href: lb_link, target: "_blank" });
+        $(location).attr({href: lb_link, target: "_blank"});
     });
 
     // portfolio more
-    const moreBtn =$("#s3 .more")
-    const pf2row =$("#s3 .pf2row")
+    const moreBtn = $("#s3 .more");
+    const pf2row = $("#s3 .pf2row");
 
-    moreBtn.click(()=>{
-        event.preventDefault();
+    moreBtn.click((e)=>{
+        // <a href="#"> -> a태그를 클릭하면 현재 페이지의 상단으로 올라간다.
+        // 기본 이벤트 방지
+        e.preventDefault();
         pf2row.css({display: "flex"});
     });
-
 
 }); // ready end
